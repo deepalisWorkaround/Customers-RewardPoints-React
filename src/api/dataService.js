af9aff4cfbabff626e74ae55c938bc1d93c2d3ba
Deepali <deepali@Deepalis-MacBook-Air.local>
2021-08-28 19:23:45 +0530

    Customers-RewardPoints-Calculation-React

---
+++ src/api/dataService.js
@@ -0,0 +1,122 @@
+
+
+export default function() {
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 157,
+          transactionDt: "08-02-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 35,
+          transactionDt: "08-02-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 84,
+          transactionDt: "08-02-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 23,
+          transactionDt: "06-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 45,
+          transactionDt: "06-21-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 198,
+          transactionDt: "07-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 11,
+          transactionDt: "07-04-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 48,
+          transactionDt: "07-03-2021"
+        },
+        {
+          custid: 1,
+          name: "Sunrise",
+          amt: 424,
+          transactionDt: "07-21-2021"
+        },
+        {
+          custid: 2,
+          name: "Royals",
+          amt: 725,
+          transactionDt: "05-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Royals",
+          amt: 76,
+          transactionDt: "08-02-2021"
+        },
+        {
+          custid: 2,
+          name: "Royals",
+          amt: 93,
+          transactionDt: "06-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Royals",
+          amt: 71,
+          transactionDt: "06-21-2021"
+        },
+        {
+          custid: 2,
+          name: "Royals",
+          amt: 220,
+          transactionDt: "07-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Royals",
+          amt: 104,
+          transactionDt: "07-21-2021"
+        },
+        {
+          custid: 3,
+          name: "Pixel Perfect",
+          amt: 300,
+          transactionDt: "01-17-2021"
+        },
+        {
+          custid: 3,
+          name: "Pixel Perfect",
+          amt: 210,
+          transactionDt: "05-22-2021"
+        },
+        {
+          custid: 3,
+          name: "Pixel Perfect",
+          amt: 156,
+          transactionDt: "09-11-2021"
+        },
+        {
+          custid: 3,
+          name: "Pixel Perfect",
+          amt: 122,
+          transactionDt: "03-12-2021"
+        }
+    ]
+  );
+};
